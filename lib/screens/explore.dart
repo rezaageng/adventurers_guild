ec81ade04ba2e466c381e6aa0211046406fe5b91
Reza Ageng <rezaageng5@gmail.com>
2022-08-16 16:12:29 +0700

    feat(explore): :sparkles: add explore screen

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../models/destinations_model.dart';
+import '../widgets/region.dart';
+
 class Explore extends StatelessWidget {
   const Explore({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Explore'),
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 240,
+          childAspectRatio: 1,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        children: const [
+          Region(
+            title: ERegions.mondstadt,
+            image: 'assets/images/wind_rise.png',
+            color: Colors.teal,
+          ),
+          Region(
+            title: ERegions.liyue,
+            image: 'assets/images/qingyun_peak.png',
+            color: Colors.amber,
+          ),
+          Region(
+            title: ERegions.inazuma,
+            image: 'assets/images/mt_yougou.png',
+            color: Colors.purple,
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/region.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/helpers.dart';
+import '../models/destinations_model.dart';
+
+class Region extends StatelessWidget {
+  final ERegions title;
+  final String image;
+  final MaterialColor color;
+
+  const Region({
+    Key? key,
+    required this.title,
+    required this.image,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      margin: EdgeInsets.zero,
+      elevation: 0,
+      child: LayoutBuilder(
+        builder: (context, constraints) => Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Image.asset(
+                  image,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      color.withOpacity(1),
+                      color.withOpacity(0.5),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              width: constraints.maxWidth,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Text(
+                  Helpers.capitalize(title.name),
+                  style: Theme.of(context).textTheme.headline6?.copyWith(
+                        color: Colors.white,
+                      ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(enableFeedback: false, onTap: () => {}),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
